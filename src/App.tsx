fd42a187a351a43dfa61e9367508b6b22df23372
andrey-ponamarev <ponamarevandrej@gmail.com>
2021-11-16 20:45:54 +0100

    fix: :lipstick: MUI, normilize css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Button from "@mui/material/Button";
 
 const App: React.FC = () => {
-  return <h1>Weather app</h1>;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <h1>Weather app</h1>
+      <Button variant="contained">Search</Button>
+    </React.Fragment>
+  );
 };
 
 export default App;
