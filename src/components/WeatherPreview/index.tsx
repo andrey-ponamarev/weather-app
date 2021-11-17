892acb32e79b6714982de53d6e4d637a4105fd31
andrey-ponamarev <ponamarevandrej@gmail.com>
2021-11-17 08:31:13 +0100

    refactor: :lipstick: Add styles to weather preview

--- src/components/WeatherPreview/index.tsx
+++ src/components/WeatherPreview/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Avatar from "@mui/material/Avatar";
 import { getIconUrl, ICityWeather } from "../../services/weather";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 interface IProps {
   data: ICityWeather;
@@ -9,23 +11,43 @@ const WeatherPreview: React.FC<IProps> = ({ data }) => {
   const { main, wind } = data;
   const currentWeather = data.weather[0];
   return (
-    <>
-      temp: {main.temp}
-      temp_max: {main.temp_max}
-      temp_min: {main.temp_min}
-      feels_like: {main.feels_like}
-      pressure: {main.pressure}
-      humidity: {main.humidity}
-      <div>
-        speed: {wind.speed}
-        deg: {wind.deg}
-      </div>
-      <Avatar
-        alt="Weather icon"
-        src={getIconUrl(currentWeather.icon)}
-        sx={{ width: 56, height: 56 }}
-      />
-    </>
+    <Grid
+      container
+      spacing={2}
+      direction="column"
+      justifyContent="center"
+      alignContent="center"
+    >
+      <Grid item>
+        <Grid container justifyContent="center" alignItems="center" spacing={2}>
+          <Grid item xs={6}>
+            <Typography variant="h3">{main.temp}°C</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Avatar
+              alt="Weather icon"
+              src={getIconUrl(currentWeather.icon)}
+              sx={{ width: 100, height: 100 }}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Typography>
+          Min/Max {main.temp_min}°C/{main.temp_max}°C
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography>
+          Feels like {main.feels_like}°C. {currentWeather.description}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography>Pressure: {main.pressure}</Typography>
+        <Typography>Humidity: {main.humidity}</Typography>
+        <Typography>Wind speed: {wind.speed}m/s</Typography>
+      </Grid>
+    </Grid>
   );
 };
 
